5f71e105e2b6784bce36e3a9a8b74d8b06e34ca6
ashstst <srivastava308@gmail.com>
2019-12-13 19:34:49 +0530

    first commit

---
+++ src/test/java/FirstMavenProject/FirstMavenProject/NewTest.java
@@ -0,0 +1,20 @@
+package FirstMavenProject.FirstMavenProject;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+@Test
+public void launchBrowser()
+{
+
+	System.out.println("Hello");
+}
+
+@Test
+
+public void launchBrowser1()
+{
+
+	System.out.println("Hello1");
+}
+}
